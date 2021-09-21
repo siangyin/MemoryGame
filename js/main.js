8a5e7094ffe424754af65f7b023ce76b2ec48d6c
Lee Siang Yin <siangyin916@gmail.com>
2021-09-22 02:05:13 +0800

    created functions to generate random cards id and created objects for games levels

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
-/** main javascript **/
-
-// function to get player name input
+/*** >> Function to get player name input ***/
 let playername;
 function getPlayerName() {
 	const playername = document.getElementById("playername").value;
 	console.log(playername);
 }
 
-// shuffle array function
-const shuffleArray = (array) => {
-	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		const temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
-	return array;
-};
+/***__Functions: >>-f->> randCardsArr (the no. of unique pairs) return random integer (0 to 80) for random cardID into an array >>-f->> randId() then >>-f->> dblCardsArr(arr) duplicate each card in given array and >>-f->> shuffleArray(arr)shuffle the cards e.g:randCardsArr(2):[5, 59, 5, 59]__***/
+function randCardsArr(num) {
+	const randId = () => {
+		return Math.floor(Math.random() * 81);
+	};
+	let array = [];
+	do {
+		let j = randId();
+		if (!array.includes(j)) {
+			array.push(j);
+		}
+	} while (array.length < num);
 
-const randArr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-const randArr2 = [...randArr1, ...randArr1];
-shuffleArray(randArr2);
-console.log(randArr2);
+	const dblCardsArr = (arr) => {
+		const shuffleArray = (arr) => {
+			for (let i = arr.length - 1; i > 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1));
+				const temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+			return arr;
+		};
+		let arrX2 = [...shuffleArray(arr), ...shuffleArray(arr)];
+		return shuffleArray(arrX2);
+	};
+	return dblCardsArr(array);
+}
+
+/*** Function: Generate Cards in <div id=id# class = cards> >>-f->> addCards(no. of card) appending into HTML page  ***/
 
 function addCards(num) {
 	for (let i = 0; i < num; i++) {
@@ -30,7 +43,30 @@ function addCards(num) {
 			.getElementById("cardbox")
 			.appendChild(newDivChild)
 			.setAttribute("id", i);
+		let docId = document.getElementById(i);
+		docId.setAttribute("class", "cards");
 	}
 }
 
-addCards(40);
+/*** Objects for Games level structures & player ***/
+
+const player = { currentlvl: 1, coins: 100, matches: 0, turns: 0 };
+
+const createLvlObj = class {
+	constructor(level, cards, time) {
+		this.level = "Level " + level;
+		this.cards = cards;
+		this.pairs = cards / 2;
+		this.time = time;
+	}
+};
+
+const gameLvl = {
+	1: { level: "Level 1", cards: 8, pairs: 4, time: 60 },
+	2: { level: "Level 2", cards: 16, pairs: 8, time: 60 },
+	3: { level: "Level 3", cards: 24, pairs: 12, time: 90 },
+	4: { level: "Level 4", cards: 32, pairs: 16, time: 90 },
+	5: { level: "Level 5", cards: 40, pairs: 20, time: 90 },
+};
+
+addCards(gameLvl[player.currentlvl].cards);
